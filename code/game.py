5321accacba9e09cc3889d8603cf98663a8a50e7
sagivt1 <sagiv.talker@gmail.com>
2023-08-06 20:34:07 +0300

    Add Block and Tetromino classes

--- code/game.py
+++ code/game.py
@@ -1,5 +1,9 @@
+from pygame.sprite import Group
+from random import choice
 import setting
 import pygame
+
+TETROMINOS_LETTER_LIST = list(setting.TETROMINOS.keys())
 
 class Game:
 
@@ -8,12 +12,16 @@ class Game:
         self.surface = pygame.Surface((setting.GAME_WIDTH, setting.GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
         self.rect = self.surface.get_rect(topleft = (setting.PADDING, setting.PADDING))
+        self.sprites = pygame.sprite.Group()
 
         # Lines surface
         self.line_surface = pygame.Surface((setting.GAME_WIDTH, setting.GAME_HEIGHT))
         self.line_surface.fill((0, 255,0))
         self.line_surface.set_colorkey((0,255,0))
         self.line_surface.set_alpha(120)
+
+        # Tetromino
+        self.tetromino = Tetromino(choice(TETROMINOS_LETTER_LIST), self.sprites)
 
     def draw_grid(self):
         for col in range(1, setting.COLUMNS):
@@ -30,8 +38,36 @@ class Game:
 
         # Drawing
         self.surface.fill(setting.GRAY)
+        self.sprites.draw(self.surface)
         
 
         self.draw_grid()
         self.display_surface.blit(self.surface, (setting.PADDING, setting.PADDING))
         pygame.draw.rect(self.display_surface, setting.LINE_COLOR, self.rect, 2, 2)
+
+class Tetromino:
+
+    def __init__(self, shape, group):
+        
+        # Setup
+        self.block_positions = setting.TETROMINOS[shape]['shape']
+        self.color = setting.TETROMINOS[shape]['color']
+
+        # Block Creation
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+
+class Block(pygame.sprite.Sprite):
+
+    def __init__(self, group, pos, color):
+
+        # Setup
+        super().__init__(group)
+        self.image = pygame.Surface((setting.CELL_SIZE,setting.CELL_SIZE))
+        self.image.fill(color)
+
+        # Position
+        self.pos = pygame.Vector2(pos) + setting.BLOCK_OFFSET
+        x = self.pos.x * setting.CELL_SIZE
+        y = self.pos.y * setting.CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x,y))
